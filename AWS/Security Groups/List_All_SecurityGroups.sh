179792cb9fc5f9f6dcddaf36a790f597bddbfdee
Rafael, Ph.D <107967743+jrafa1607@users.noreply.github.com>
2025-11-01 21:25:18 -0300

    Update List_All_SecurityGroups.sh

--- AWS/Security Groups/List_All_SecurityGroups.sh
+++ AWS/Security Groups/List_All_SecurityGroups.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 contas=`cat contas`
 # Declare an array of string with type
-declare -a StringArrayRegions=("us-east-1" "us-east-2" "us-west-2" "sa-east-1" )
+declare -a StringArrayRegions=("us-east-1" "sa-east-1" )
 for conta in $contas;
 do
         for regiao in ${StringArrayRegions[@]}; # Iterate the string array using for loop
         do
-                echo "==== Verificando Ec2 da conta: $conta"
+                echo "==== Check the Ec2 from Account: $conta"
                 cmd=$(aws ec2 describe-network-interfaces  --filters Name=addresses.private-ip-address,Values=x.x.x.x --output text --region $regiao --profile $conta)
-                echo "$conta - $regiao - $cmd" | tee -a ListSG.txt
+                echo "$conta - $regiao - $cmd" | tee -a ListSecurityGroups.txt
                 echo "================================="
         done
 done
