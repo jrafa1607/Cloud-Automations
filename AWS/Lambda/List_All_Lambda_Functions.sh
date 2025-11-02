a7722ed5e9b4cef3d37348385d2a602aeea7cda2
Rafael, Ph.D <107967743+jrafa1607@users.noreply.github.com>
2025-11-01 21:15:30 -0300

    Update List_All_Lambda_Functions.sh

--- AWS/Lambda/List_All_Lambda_Functions.sh
+++ AWS/Lambda/List_All_Lambda_Functions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 contas=`cat contas`
 # Declare an array of string with type
-declare -a StringArrayRegions=("us-east-1" "us-east-2" "us-west-2" "sa-east-1" )
+declare -a StringArrayRegions=("us-east-1" "sa-east-1" )
 for conta in $contas;
 do
         for regiao in ${StringArrayRegions[@]}; # Iterate the string array using for loop
