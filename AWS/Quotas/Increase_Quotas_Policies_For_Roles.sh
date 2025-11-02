23db5ab9b5565f62dda85de5f0dd8e420dc0f4f9
Rafael, Ph.D <107967743+jrafa1607@users.noreply.github.com>
2025-11-01 21:01:18 -0300

    Update Increase_Quotas_Policies_For_Roles.sh

--- AWS/Quotas/Increase_Quotas_Policies_For_Roles.sh
+++ AWS/Quotas/Increase_Quotas_Policies_For_Roles.sh
@@ -6,7 +6,7 @@ for conta in $contas;
 do
         for regiao in ${StringArrayRegions[@]}; # Iterate the string array using for loop
         do
-                echo "==== Solicitando o aumento de Quotas para Roles IAM = 10 para 20 Policies Atachadas na conta: $conta na regiao $regiao ======"
+                echo "==== Request the Quotas Update - IAM Managed Policies from 10 to 20: $conta na regiao $regiao ======"
                 docsdb=$(aws service-quotas request-service-quota-increase --service-code iam --quota-code L-0DA4ABF3 --desired-value 20 --region $regiao --profile $conta)
                 echo "$conta - $regiao - $docsdb" | tee -a report_quotas_increase.txt
                 echo "========================================================================================"
