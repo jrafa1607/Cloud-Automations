8d42ffc3ff7b41686c65441a611b05429b617528
Rafael, Ph.D <107967743+jrafa1607@users.noreply.github.com>
2025-11-01 21:17:35 -0300

    Update Increase_Quotas_VPCs_For_Regions.sh

--- AWS/Quotas/Increase_Quotas_VPCs_For_Regions.sh
+++ AWS/Quotas/Increase_Quotas_VPCs_For_Regions.sh
@@ -6,7 +6,7 @@ for conta in $contas;
 do
         for regiao in ${StringArrayRegions[@]}; # Iterate the string array using for loop
         do
-                echo "==== Solicitando o aumento de Quotas de VPCs de 20 Por regioes para 25 na conta: $conta na regiao $regiao ======"
+                echo "==== Request the Increase of VPC Quotas from 20 to 25 by each Region in the Account: $conta in the region: $regiao ======"
                 docsdb=$(aws service-quotas request-service-quota-increase --service-code vpc --quota-code L-F678F1CE --desired-value 25 --region $regiao --profile $conta)
                 echo "$conta - $regiao - $docsdb" | tee -a report_quotas_increase.txt
                 echo "========================================================================================"
