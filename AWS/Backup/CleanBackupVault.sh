8353a79f581044ca7dfba4aca2371f059dbeb1f4
Rafael, Ph.D <107967743+jrafa1607@users.noreply.github.com>
2025-11-02 21:12:01 -0300

    Update CleanBackupVault.sh

--- AWS/Backup/CleanBackupVault.sh
+++ AWS/Backup/CleanBackupVault.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-contas=`cat contas`
+accounts=`cat accounts`
 
 echo "Vault Name: "
 read vault
@@ -7,19 +7,19 @@ read vault
 echo Region: "
 read region
 
-for conta in $contas;
+for account in $accounts;
 do
     echo "========================================================"
-    echo "Check all The Recovery Points in the Vault: $vault in the AWS Account: $conta"
+    echo "Check all The Recovery Points in the Vault: $vault in the AWS Account: $account"
 
-	list=$(aws backup list-recovery-points-by-backup-vault --query 'RecoveryPoints[].[RecoveryPointArn]' --backup-vault-name $vault --region $region --profile $conta --output json > RecoveryPointsList)
+	list=$(aws backup list-recovery-points-by-backup-vault --query 'RecoveryPoints[].[RecoveryPointArn]' --backup-vault-name $vault --region $region --profile $account --output json > RecoveryPointsList)
 	
 	rp=`cat RecoveryPointsList`
 
 	for point in $rp;
 	do
 		echo "Removing the Recovery Point: $point"
-		removerp=$(aws backup delete-recovery-point --recovery-point-arn $point --backup-vault-name $vault --output text --region $region --profile $conta)
+		removerp=$(aws backup delete-recovery-point --recovery-point-arn $point --backup-vault-name $vault --output text --region $region --profile $account)
 		
 		echo $removerp		
        	done
