2eb388d253654a24bdfb3d4c6ba632808c26be1d
Rafael, Ph.D <107967743+jrafa1607@users.noreply.github.com>
2025-11-02 21:22:47 -0300

    Update Get_All_Trails_Enabled.sh

--- AWS/Cloudtrail/Get_All_Trails_Enabled.sh
+++ AWS/Cloudtrail/Get_All_Trails_Enabled.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/bash
-contas=`cat contas`
+accounts=`cat accounts`
+
 # Declare an array of string with type
 declare -a StringArrayRegions=("us-east-1" "sa-east-1" )
-for conta in $contas;
+
+for account in $accounts;
 do
-        for regiao in ${StringArrayRegions[@]}; # Iterate the string array using for loop
+        for region in ${StringArrayRegions[@]}; # Iterate the string array using for loop
         do
-                echo "==== All Trails in the Account: $conta"
-                cmd=$(aws cloudtrail list-trails --output text --region $regiao --profile $conta)
+                echo "==== All Trails in the Account: $account"
+                cmd=$(aws cloudtrail list-trails --output text --region $region --profile $account)
                 
-		echo "$conta - $regiao - $cmd" | tee -a trails.txt
+		echo "$account - $region - $cmd" | tee -a trails.txt
                 echo "================================="
         done
 done
