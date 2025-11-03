1b2543ddc2755b6f720cf284e55faf7abffa1830
Rafael, Ph.D <107967743+jrafa1607@users.noreply.github.com>
2025-11-02 21:42:20 -0300

    Update GetIAM.sh

--- AWS/IAM/GetIAM.sh
+++ AWS/IAM/GetIAM.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/bash
-contas=`cat contas`
+accounts=`cat accounts`
 
-for conta in $contas;
+for account in $accounts;
 do
         echo "========================================================"
-        echo "Check all The Users from the AWS Account: $conta"
+        echo "Check all The Users from the AWS Account: $account"
 
-        list=$(aws iam list-users --profile $conta --query 'Users[].[UserName]' --output text > users)
+        list=$(aws iam list-users --profile $account --query 'Users[].[UserName]' --output text > users)
         echo -e "\n- All the Users Listed: "
 
-        usr=$(aws iam list-users --profile $conta --query 'Users[].[UserName]' --output text)
+        usr=$(aws iam list-users --profile $account --query 'Users[].[UserName]' --output text)
         echo "$usr"
 
         users=`cat users`
         for user in $users;
         do
 
-                getstatus=$(aws iam list-access-keys --user-name $user --profile $conta  --output json)
-                getkeys=$(aws iam list-access-keys --user-name $user --profile $conta --query 'AccessKeyMetadata[].[AccessKeyId]' --output text > keys)
+                getstatus=$(aws iam list-access-keys --user-name $user --profile $account  --output json)
+                getkeys=$(aws iam list-access-keys --user-name $user --profile $account --query 'AccessKeyMetadata[].[AccessKeyId]' --output text > keys)
 
                 echo -e "\nInfo About All the Keys for the user: $user"
                 echo "$getstatus"
@@ -26,7 +26,7 @@ do
                 keys=`cat keys`
                 for key in $keys;
                 do
-                        listkey=$(aws iam get-access-key-last-used --access-key-id $key --profile $conta --output json)
+                        listkey=$(aws iam get-access-key-last-used --access-key-id $key --profile $account --output json)
                         echo "Information About the Last Access of the Key: $key"
                         echo "$listkey"
                         echo "==========*==========="
