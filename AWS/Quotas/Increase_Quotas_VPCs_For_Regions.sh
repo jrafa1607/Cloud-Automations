28b33f12a77c79ac2af7cca22b0f397d6bdfa05b
Rafael, Ph.D <107967743+jrafa1607@users.noreply.github.com>
2025-11-04 09:10:31 -0300

    Update Increase_Quotas_VPCs_For_Regions.sh

--- AWS/Quotas/Increase_Quotas_VPCs_For_Regions.sh
+++ AWS/Quotas/Increase_Quotas_VPCs_For_Regions.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/bash
-contas=`cat contas`
+accounts=`cat accounts`
+
 # Declare an array of string with type
-declare -a StringArrayRegions=("us-east-1" "sa-east-1")
-for conta in $contas;
+declare -a StringArrayRegions=("us-east-1" "sa-east-1" )
+
+for account in $accounts;
 do
-        for regiao in ${StringArrayRegions[@]}; # Iterate the string array using for loop
+        for region in ${StringArrayRegions[@]}; # Iterate the string array using for loop
         do
-                echo "==== Request the Increase of VPC Quotas from 20 to 25 by each Region in the Account: $conta in the region: $regiao ======"
-                docsdb=$(aws service-quotas request-service-quota-increase --service-code vpc --quota-code L-F678F1CE --desired-value 25 --region $regiao --profile $conta)
-                echo "$conta - $regiao - $docsdb" | tee -a report_quotas_increase.txt
+                echo "==== Request the Increase of VPC Quotas from 20 to 25 in the Account: $account in the region: $region ======"
+                docsdb=$(aws service-quotas request-service-quota-increase --service-code vpc --quota-code L-F678F1CE --desired-value 25 --region $region --profile $account)
+                
+                echo "$account - $region - $docsdb" | tee -a report_quotas_increase.txt
                 echo "========================================================================================"
         done
 done
