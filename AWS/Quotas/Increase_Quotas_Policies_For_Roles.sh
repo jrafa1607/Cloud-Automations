ff1037f82d7ba8bb6c67fffe8e5773076f5d4484
Rafael, Ph.D <107967743+jrafa1607@users.noreply.github.com>
2025-11-04 09:09:18 -0300

    Update Increase_Quotas_Policies_For_Roles.sh

--- AWS/Quotas/Increase_Quotas_Policies_For_Roles.sh
+++ AWS/Quotas/Increase_Quotas_Policies_For_Roles.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/bash
-contas=`cat contas`
+accounts=`cat accounts`
+
 # Declare an array of string with type
-declare -a StringArrayRegions=("us-east-1" "sa-east-1")
-for conta in $contas;
+declare -a StringArrayRegions=("us-east-1" "sa-east-1" )
+
+for account in $accounts;
 do
-        for regiao in ${StringArrayRegions[@]}; # Iterate the string array using for loop
+        for region in ${StringArrayRegions[@]}; # Iterate the string array using for loop
         do
-                echo "==== Request the Quotas Update - IAM Managed Policies from 10 to 20: $conta in the Region: $regiao ======"
-                docsdb=$(aws service-quotas request-service-quota-increase --service-code iam --quota-code L-0DA4ABF3 --desired-value 20 --region $regiao --profile $conta)
-                echo "$conta - $regiao - $docsdb" | tee -a report_quotas_increase.txt
+                echo "==== Request the Quotas Update - IAM Managed Policies from 10 to 20: $account in the Region: $region ======"
+                docsdb=$(aws service-quotas request-service-quota-increase --service-code iam --quota-code L-0DA4ABF3 --desired-value 20 --region $region --profile $account)
+                
+                echo "$account - $region - $docsdb" | tee -a report_quotas_increase.txt
                 echo "========================================================================================"
         done
 done
