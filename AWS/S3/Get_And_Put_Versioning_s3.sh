7c18bc72e0f33ffdd6dd7c98c83a1390b57ca9ff
Rafael, Ph.D <107967743+jrafa1607@users.noreply.github.com>
2025-11-05 09:29:33 -0300

    Update Get_And_Put_Versioning_s3.sh

--- AWS/S3/Get_And_Put_Versioning_s3.sh
+++ AWS/S3/Get_And_Put_Versioning_s3.sh
@@ -1,31 +1,29 @@
 #!/usr/bin/bash
+accounts=`cat accounts`
 
-contas=`cat contas`
-
-for conta in $contas;
+for account in $accounts;
 do
         echo "========================================================"
-        echo "Check the s3 Buckets in the Account: $conta"
+        echo "Check the s3 Buckets in the Account: $account"
 
-        list=$(aws s3api list-buckets --query "Buckets[].Name" --output text --profile $conta > buckets)
+        list=$(aws s3api list-buckets --query "Buckets[].Name" --output text --profile $account > buckets)
         buckets=`cat buckets`
 
         for bucket in $buckets;
         do
-
-                cmd=$(aws s3api get-bucket-versioning --bucket $bucket --output text --profile $conta)
+                cmd=$(aws s3api get-bucket-versioning --bucket $bucket --output text --profile $account)
 
                 if [ -z "$cmd" ]; then
-                        verscmd=$(aws s3api put-bucket-versioning --bucket $bucket --versioning-configuration Status=Enabled --profile $conta)
-                        echo "$conta - $bucket - "Disabled --NewStatus: Enabled"" | tee -a buckets-list-updated.txt
+                        verscmd=$(aws s3api put-bucket-versioning --bucket $bucket --versioning-configuration Status=Enabled --profile $account)
+                        echo "$account - $bucket - "Disabled --NewStatus: Enabled"" | tee -a buckets-list-updated.txt
                 elif [ $cmd == "Suspended" ]; then
-                        verscmd=$(aws s3api put-bucket-versioning --bucket $bucket --versioning-configuration Status=Enabled --profile $conta)
-                        echo "$conta - $bucket - $cmd "--NewStatus: Enabled"" | tee -a buckets-list-updated.txt
+                        verscmd=$(aws s3api put-bucket-versioning --bucket $bucket --versioning-configuration Status=Enabled --profile $account)
+                        echo "$account - $bucket - $cmd "--NewStatus: Enabled"" | tee -a buckets-list-updated.txt
                 elif [ $cmd == "Disabled" ]; then
-                        verscmd=$(aws s3api put-bucket-versioning --bucket $bucket --versioning-configuration Status=Enabled --profile $conta)
-                        echo "$conta - $bucket - $cmd "--NewStatus: Enabled"" | tee -a buckets-list-updated.txt
+                        verscmd=$(aws s3api put-bucket-versioning --bucket $bucket --versioning-configuration Status=Enabled --profile $account)
+                        echo "$account - $bucket - $cmd "--NewStatus: Enabled"" | tee -a buckets-list-updated.txt
                 else
-                        echo "$conta - $bucket - $cmd" | tee -a buckets-list.txt
+                        echo "$account - $bucket - $cmd" | tee -a buckets-list.txt
 
                 fi
         done
