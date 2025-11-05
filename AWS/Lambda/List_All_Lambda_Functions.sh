bd6f5d0d69993a07efed854c2d14809eab1ebc0e
Rafael, Ph.D <107967743+jrafa1607@users.noreply.github.com>
2025-11-05 09:25:46 -0300

    Update List_All_Lambda_Functions.sh

--- AWS/Lambda/List_All_Lambda_Functions.sh
+++ AWS/Lambda/List_All_Lambda_Functions.sh
@@ -2,16 +2,17 @@
 accounts=`cat accounts`
 
 # Declare an array of string with type
-declare -a StringArrayRegions=("us-east-1" "sa-east-1" )
+declare -a StringArrayRegions=("us-east-1" "sa-east-1")
 
 for account in $accounts;
 do
         for region in ${StringArrayRegions[@]}; # Iterate the string array using for loop
         do
                 echo "==== List All Lambda Functions from Account: $account"
-                cmd=$(aws lambda list-functions --query 'Functions[].[FunctionName,FunctionArn]' --output json --region $region --profile $account)
-                echo "$account - $region" 
+				cmd=$(aws lambda list-functions --query 'Functions[].[FunctionName,FunctionArn]' --output json --region $region --profile $account)
+				echo "$account - $region" 
+				
 		echo "$cmd"
-		echo "================================================================================="
+		echo "============================================================================="
         done
 done
